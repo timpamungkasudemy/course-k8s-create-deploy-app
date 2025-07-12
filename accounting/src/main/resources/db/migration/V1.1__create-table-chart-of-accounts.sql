b9b6972e500368c131575a1d3150ca083eb063de
timpamungkas <timotius.pamungkas@gmail.com>
2025-07-12 09:00:00 +0700

    chore: add H2

--- accounting/src/main/resources/db/migration/V1.1__create-table-chart-of-accounts.sql
+++ accounting/src/main/resources/db/migration/V1.1__create-table-chart-of-accounts.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS chart_of_accounts (
-	coa_id VARCHAR NOT NULL,
+	coa_id VARCHAR(255) NOT NULL,
 	company VARCHAR(255),
 	cost_center VARCHAR(255),
 	natural_account VARCHAR(255),
